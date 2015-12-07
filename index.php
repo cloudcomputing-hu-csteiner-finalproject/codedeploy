f333bd0c8aa6b733ae12fd1eb923affa31cc46f5
Christine Steiner <christinesteiner@Christines-MacBook-Pro-3.local>
2015-12-07 20:59:54 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+print "<h2>Sorry not enough time to create interesting content!</h2>";
+print "Hello world!<br>";
+print "I'm learning codedeploy!";
+?>  
+<h3>Okay here's something interesting </h3>
+<img src="lego_term.jpg" alt="Lego Terminal" style="width:800px;height:600px;">
+
+<h2>This application was deployed using AWS CodeDeploy.</h2>
+
+</body>
+</html>
